5eb9d9c58f8a10a19fc2e24c5d681ea59fc618f6
PRMTRG <trgprm@gmail.com>
2022-12-28 18:03:36 +0100

    Clarify Watchdog behavior

--- watchdog.h
+++ watchdog.h
@@ -5,8 +5,8 @@ void * watchdog_run(void *arg);
 
 /*
  * Signal that the thread is still active.
- * If a watched thread doesn't report activity for more than 2 seconds
- * the program will exit.
+ * If a watched thread doesn't report activity for more than 2 seconds Watchdog
+ * will cancel all watched threads and exit.
  * Using this function automatically adds the thread to the list of watched threads.
  *
  * name must be a valid C string containing the name of the thread that
